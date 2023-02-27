5f233b45ac3971e9ab1f337f819935c03722dc01
Richard Whitehill <whiteric@jlab.org>
2023-02-27 13:09:37 -0600

    update

--- Homework/Homework3/main.cpp
+++ Homework/Homework3/main.cpp
@@ -1,9 +1,9 @@
+// import functionality from other files
 #include <iostream>
 #include <list>
-#include <iterator>
-#include <string>
 #include <fstream>
 
+// print list from head to tail using iterators
 template <class T>
 void print_list(std::list<T> lst) {
     for (auto it = lst.begin(); it != lst.end(); it++) {
@@ -12,6 +12,7 @@ void print_list(std::list<T> lst) {
     std::cout << std::endl;
 }
 
+// print list from tail to head using reverse iterators
 template <class T>
 void print_list_num(std::list<T> lst) {
     for (auto it = lst.rbegin(); it != lst.rend(); it++) {
@@ -21,28 +22,37 @@ void print_list_num(std::list<T> lst) {
 
 }
 
+// function to add two lists together
 std::list<int> add_lists(std::list<int> list1, std::list<int> list2) {
+    // initialize result list and iterators for both lists
     std::list<int> res;
     auto it1 = list1.begin();
     auto it2 = list2.begin();
-   
+  
+    // loop to add numbers in list
+    // tens holds the carry-over for place-value addition
     int tens = 0;
     while (it1 != list1.end() || it2 != list2.end()) {
         int num1 = 0;
         int num2 = 0;
+        // get digit from first list and increment iterator if not end
         if (it1 != list1.end()) {
             num1 = *it1;
             it1++;
         }
+        // get digit from second list and increment iterator if not end
         if (it2 != list2.end()) {
             num2 = *it2;
             it2++;
         }
+        // get place-value for current digit by adding carry-over and
+        // digits from lists
         int sum = num1 + num2 + tens;
         res.push_back(sum % 10);
 
         tens = (sum / 10)*(sum >= 10);
     }
+    // for largest digit if carry-over from last operation add to list
     if (tens != 0) {
         res.push_back(tens);
     }
@@ -52,23 +62,30 @@ std::list<int> add_lists(std::list<int> list1, std::list<int> list2) {
 
 int main() {
   
+    // initialize file object and open file with numbers to add
     std::fstream file;
     file.open("nums.txt");
 
+   
+    // initialize line variable to read into
+    // and sum as 0
     std::string line;
     std::list<int> sum; 
     sum.push_back(0);
 
-    while (file >> line) {
+    // read numbers from file (into curr), add to sum and store as new sum
+    while (std::getline(file,line)) {
         std::list<int> curr;
         for (auto i = 1; i <= line.length(); i++) {
             curr.push_back( (int) line[line.length()-i] - '0');
         }
         sum = add_lists(sum,curr);
     }
-
+    
+    // close file
     file.close();
 
+    // print out resultant sum
     print_list_num(sum);
 
     return 0;
--- Lab/Lab3/main.cpp
+++ Lab/Lab3/main.cpp
@@ -1,9 +1,12 @@
+// import functionality from other files
 #include <iostream>
 #include "Node.h"
 #include "LinkedList.h"
 
+// define NodePtr type
 typedef Node* NodePtr;
 
+// helper function to print out values in linked list (takes in head node)
 void print_nodes(NodePtr head) {
     auto temp = head;
     while (temp != nullptr) {
@@ -13,45 +16,59 @@ void print_nodes(NodePtr head) {
     std::cout << std::endl;
 }
 
+// takes in 2 linked list node pointers and returns node pointer to sorted
+// linked list
 NodePtr question1(NodePtr a, NodePtr b) {
+    // initialize result node pointer and temp variable for iteration
     NodePtr ret = new Node(0);
     NodePtr temp = ret;
 
+    // initialize temp variables for iterating through input linked lists
     NodePtr temp_a = a;
     NodePtr temp_b = b;
+    // loop through linked lists while not at end of both
     while (temp_a != nullptr || temp_b != nullptr) {
+        // if at end of either linked list just add all elements of 
+        // other list to the end of res
         if (temp_a == nullptr) {
-            //std::cout << temp_b->n << " ";
             temp->next = new Node(temp_b->n);
             temp_b = temp_b->next;
         }
         else if (temp_b == nullptr) {
-            //std::cout << temp_a->n << " ";
             temp->next = new Node(temp_a->n);
             temp_a = temp_a->next;
         }
+        // if not at end of either list: work through lists
+        // compare current values of list --> add larger to res and update 
+        // corresponding node pointer
         else {
             if (temp_a->n < temp_b->n) {
-                //std::cout << temp_a->n << " ";
                 temp->next = new Node(temp_a->n);
                 temp_a = temp_a->next;
             }
             else {
-                //std::cout << temp_b->n << " ";
                 temp->next = new Node(temp_b->n);
                 temp_b = temp_b->next; 
             }
             temp = temp->next;
         }
     }
+
+    // return node pointer for sorted list (next node because of how
+    // result was initialized)
     return ret->next;
 }
 
+// takes in node pointer for list and returns "sorted" list pointer
+// sort: keep relative order but move odd numbers to front
 NodePtr question2(NodePtr head) {
+    // initialize result node pointer, temp for iteration,
+    // and temp variable for iterating through input node pointer
     NodePtr ret = new Node(0);
     NodePtr temp = ret;
     NodePtr temp_head = head;
 
+    // loop through input node pointer and append odd numbers to result list
     while (temp_head != nullptr) {
         if (temp_head->n % 2 == 1) {
             temp->next = new Node(temp_head->n);
@@ -59,7 +76,9 @@ NodePtr question2(NodePtr head) {
         }
         temp_head = temp_head->next;
     }
-    
+   
+    // reinitialize head node temp variable and repeat loop above but
+    // for even numbers
     temp_head = head;
     while (temp_head != nullptr) {
         if (temp_head->n % 2 == 0) {
@@ -68,7 +87,9 @@ NodePtr question2(NodePtr head) {
         }
         temp_head = temp_head->next;
     }
-
+    
+    // return next node in result node pointer (because of how it was
+    // initialized)
     return ret->next;
 }
 
@@ -76,6 +97,9 @@ NodePtr question2(NodePtr head) {
 int main() {
 
     // Question 1 example
+    // input 1: { 1,3,5,7 }
+    // input 2: { 2,4,6,8 }
+    // expected output: { 1,2,3,4,5,6,7,8 }
     NodePtr a = new Node(1);
     a->next = new Node(3);
     a->next->next = new Node(5);
@@ -88,7 +112,11 @@ int main() {
 
     print_nodes(question1(a,b));
 
+    std::cout << std::endl;
+
     // Question 2 example
+    // input: { 1,2,5,8,3,6,7,4 }
+    // expected output: { 1,5,3,7,2,8,6,4 }
     NodePtr c = new Node(0);
     NodePtr temp = c;
     for (auto i : { 1,2,5,8,3,6,7,4 }) {
@@ -98,7 +126,6 @@ int main() {
     c = c->next;
 
     print_nodes(question2(c));
-    print_nodes(c);
 
     return 0;
 }
